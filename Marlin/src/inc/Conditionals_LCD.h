63edd9342d31708cd50fe1b6edf8aac5efb20093
miferkolb <61791039+miferkolb@users.noreply.github.com>
2020-10-02 18:38:28 +0200

    Comented LCD for bl touch

--- Marlin/src/inc/Conditionals_LCD.h
+++ Marlin/src/inc/Conditionals_LCD.h
@@ -532,12 +532,12 @@
   #endif
 
   // Always disable probe pin inverting for BLTouch
-  #undef Z_MIN_PROBE_ENDSTOP_INVERTING
-  #define Z_MIN_PROBE_ENDSTOP_INVERTING true
-  #if ENABLED(Z_MIN_PROBE_USES_Z_MIN_ENDSTOP_PIN)
-    #undef Z_MIN_ENDSTOP_INVERTING
-    #define Z_MIN_ENDSTOP_INVERTING true
-  #endif
+  //#undef Z_MIN_PROBE_ENDSTOP_INVERTING
+  //#define Z_MIN_PROBE_ENDSTOP_INVERTING false
+  //#if ENABLED(Z_MIN_PROBE_USES_Z_MIN_ENDSTOP_PIN)
+    //#undef Z_MIN_ENDSTOP_INVERTING
+    //#define Z_MIN_ENDSTOP_INVERTING false
+  //#endif
 #endif
 
 #ifndef NUM_SERVOS
